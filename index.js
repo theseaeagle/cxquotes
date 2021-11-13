47a11fc895ad5d69493ce902b9376ff0cd9a3507
Mohamed Shaamil <145243+theseaeagle@users.noreply.github.com>
2021-11-13 21:29:30 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-var express = require("express");
+var express = require('express');
 var rp = require('request-promise');
 var unirest = require('unirest');
-
-const express_app = express()
-const port = 3000
-
-express_app.set("view engine", "ejs");
-
+var app = express();
+app.set("view engine", "ejs");
 
 //Database
 var fs = require('fs');
@@ -17,73 +13,80 @@ var db = new sqlite3.Database(dbFile);
 
 // if ./.data/sqlite.db does not exist, create it, otherwise print records to console
 db.serialize(function(){
-  if (!exists) {
-    db.run('CREATE TABLE Track (id INTEGER PRIMARY KEY, lastNumber INTEGER)');
+  if (exists) {
+    //db.run('CREATE TABLE Track (id INTEGER PRIMARY KEY, lastNumber INTEGER)');
+	db.prepare(`CREATE TABLE IF NOT EXISTS Track (id INTEGER PRIMARY KEY, lastNumber INTEGER)`).run().finalize();
     console.log('New table Track created!');
-    
-    // insert default dreams
-    db.serialize(function() {
-      db.run('INSERT INTO Track (id,lastNumber) VALUES (1,1);
-    });
-  }
-  else {
-    console.log('Database "Dreams" ready to go!');
-    db.each('SELECT * from Dreams', function(err, row) {
-      if ( row ) {
-        console.log('record:', row);
-      }
-    });
+	//insert defaultStatus
+	
+	db.run(`INSERT INTO Track (id,lastNumber) VALUES(?,?)`, [1,1], function(err) {
+		if (err) {
+		  //return console.log(err.message);
+		}
+		// get the last insert id
+		console.log(`A row has been inserted with rowid ${this.lastID}`);
+	});
+
   }
 });
-
 //End Database
 
+//function getQuote(quoteFromDB){
+const getQuote = new Promise((success) =>	{
+    db.serialize(function(callback) {
+		var quoteFromDB="";
+		db.get("SELECT * FROM Track WHERE Id = 1", (err, row)=>{
+			var lastnumber= parseInt(row.lastNumber,10);
+			var nextnumber = lastnumber+1;
+			var updatesql = 'UPDATE Track SET  lastNumber=' + nextnumber + ' WHERE id=1';
+			db.run(updatesql,function(err) { 
+				if (err || this.changes==0) {
+					//console.error(err.message);
+					var insertsql = 'INSERT INTO Track(id,lastNumber)  VALUES(1,1)';
+					db.run(insertsql);
+				}
+				console.log('Row(s) updated: ${this.changes}');
+				//return;
+			});
+			//Get Quotes
+			db.get("SELECT * FROM quotes WHERE Id =" + lastnumber, (err, row)=>{
+				//console.log(row);
+				//create json
+				var o = {} // empty Object
+				var key = 'data';
+				o[key] = []; // empty Array, which you can push() values into
 
 
-
-function updateDB(){
-    db.serialize(function() {
-        var updatesql = 'UPDATE Track SET  lastNumber=' + lastnumber+1 + 'WHERE id=1';
-        db.run(updatesql,function(err) { 
-          if (err || this.changes==0) {
-            //console.error(err.message);
-            var insertsql = 'INSERT INTO Track(id,lastNumber)  VALUES(1,1)';
-            db.run(insertsql);
-          }
-            console.log('Row(s) updated: ${this.changes}');
-            return;
-        });
+				var data = {
+					quoteAuthor: row.quoteAuthor,
+					quoteText: row.quoteText
+				};
+				o[key].push(data);
+				//console.log(JSON.stringify(o));
+				//quoteFromDB = JSON.stringify(o);
+				success(o);
+			});
+		});
+		//console.log(quoteFromDB);
    });
-}
-
-
-
-
-express_app.get('/', function(request, response) {
-  user = request.query.email;
-  db.all('SELECT * from Dreams WHERE user="'+ user + '"', function(err, rows) {
-    response.send(JSON.stringify(rows));
-  });
-  
-});
-
-express_app.get('/tos', function(request, response) {
-  response.sendFile(__dirname + '/views/tos.html');
-});
-
-express_app.get('/pp', function(request, response) {
-  response.sendFile(__dirname + '/views/pp.html');
-});
-
-express_app.get('/songs', function(request, response) {
-  response.sendFile(__dirname + '/songs/NoTearsLeftToCry.mp3');
+   
+   
 });
 
 
-express_app.get('/uploadsongs', function(request, response) {
-  response.sendFile(__dirname + '/managesongs/managesongs.html');
+app.get('/', function (req, res) {
+	getQuote.then(function(result){
+		ress = result; // Now you can use res everywhere
+		
+	});
+	
+	getQuote.then(res.send.bind(res));
+
+	//console.log(getQuote.result);
+    //res.send();
 });
 
 
-
-express_app.listen(PORT, () => console.log("Listening on port " + PORT + "."));
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
